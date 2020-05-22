73c3916822670dfbfee7452ad84619325c2d430f
stickler-ci <support@stickler-ci.com>
2020-05-22 08:10:42 +0000

    Fixing style errors.

--- test_emission_trading.py
+++ test_emission_trading.py
@@ -5,12 +5,13 @@ def test_buy_or_sell():
     assert buy_or_sell(500, 250) == True
 
 
-#def test_buy_or_sell():
+# def test_buy_or_sell():
 #     assert buy_or_sell(100, 250) == print('The company can sell allocations')
 
 
 def test_trading():
-    assert trading(20, 15)== True
+    assert trading(20, 15) == True
+
 
 def test_trading():
     assert trading(20, 15) == print('Trading is possible...')
